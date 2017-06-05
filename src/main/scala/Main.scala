88f35b21f9fbba0f061635c4f957e0751496f421
Lewis Hay <lewis.hay@qa.com>
2017-06-05 12:05:17 +0000

    Hey world & time

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,14 @@
+import java.util.Calendar
+
 /**
   * Created by Administrator on 05/06/2017.
   */
 object Main {
   def main(args: Array[String]) = {
-    println("Hey world")
+    for(i<-1 to 3; j<-1 to 3) {
+      println("Hey world " + i + "." + j)
+    }
+    val theDate = Calendar.getInstance()
+    println("The current date is: " + theDate.getTime())
   }
 }
